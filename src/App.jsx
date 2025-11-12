845ee71121e04ba0087886e1a52e6228d3f498d3
miaravnanger <mia_maroen@hotmail.com>
2025-11-12 15:46:56 +0100

    r-oppgave2

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import CookieCounter from "./components/CookieClicker.jsx"
+import './App.css'
+
+export default function App() {
+  return <>
+  <CookieCounter/>
+  </>;
+}
+
---
+++ src/components/CookieClicker.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import styles from "./styles/CookieClicker.module.css"
+
+export default function CookieCounter() {
+  const [count, setCount] = useState(0);
+  const handleIncrease = () => {
+    setCount((prev)=> prev + 1);
+  };
+
+  return (
+    <div className="cookieBody">
+      <h2>Click me!</h2>
+      <h3>{count}</h3>
+      <button onClick={handleIncrease}>
+        <img src="../../public/cookie2.png" alt="image of a cookie" width="60px" />
+      </button>
+    </div>
+  );
+}
