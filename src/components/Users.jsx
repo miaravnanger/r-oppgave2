9a743202adf315761bada93b118350aef8567288
miaravnanger <mia_maroen@hotmail.com>
2025-11-13 11:22:16 +0100

    users update

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,20 +1,30 @@
 import { useState } from "react"
-import { mockData } from "./data";
+
 
 
 export default function Users() {
-    const [userData, setUserdata] = useState(mockData);
+    const mockData = [
+      { username: "Ola Normann", email: "ola.normann@norge.no" },
+      { username: "Torleif", email: "torleif@kodehode.no" },
+      { username: "Jan Egil", email: "jan.egil@kodehode.no" },
+      { username: "Sander", email: "sander@kodehode.no" },
+    ];
+
+    const [users, setUsers] = useState(mockData);
     
 
 return (
     <>
     <div>
         <h2>Users:</h2>
-        <ul>
-        {mockData.map((user, i) => (
-            <li key={i}>{mockData.username}</li>
-        ))}
-        </ul>
+{users.map((user, i) => {
+    return (
+        <div key={i}>
+            <p>Name: {user.username}</p>
+            <p>Email: {user.email}</p>
+        </div>
+    )
+})}
     </div>
     </>
 )
--- src/components/data.js
+++
@@ -1,6 +0,0 @@
-export const mockData = [
-  { username: "Ola Normann", email: "ola.normann@norge.no" },
-  { username: "Torleif", email: "torleif@kodehode.no" },
-  { username: "Jan Egil", email: "jan.egil@kodehode.no" },
-  { username: "Sander", email: "sander@kodehode.no" },
-];
